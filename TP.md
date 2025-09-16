5b1aa8f125914bf0ffffdbc536a5795db48bdab9
Antoine-Ali Zarrouk <3798576+sidewinder94@users.noreply.github.com>
2025-09-16 14:45:09 +0200

    Initialize tp_4

---
+++ TP.md
@@ -0,0 +1,9 @@
+# TP 4
+
+- Récupérer en local la branche tp_4
+
+- Dans le sous-module, créer une branche personnelle au format suivant : tp_[trois_premiere_lettre_du_nom]_4
+
+- Besoin client : mettre le lien dans un élément bouton et lui donner un style bord arrondi et fond de couleur
+
+
---
+++ src/business.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+class ProductType(Enum):
+    TYPE_A = "type_a"
+    TYPE_B = "type_b"
+    TYPE_C = "type_c"
+
+
+class Product:
+    def __init__(self, name: str, product_type: ProductType, price: int):
+        self.name = name
+        self.product_type = product_type
+        self.price = price
+
+
+class NotEnoughMoney(Exception):
+    pass
+
+
+class Client:
+    def __init__(self, name: str, wallet: int):
+        self.name = name
+        self.wallet = wallet
+
+    def buy_product(self, product: Product, quantity: int = 1):
+        if self.wallet < product.price * quantity:
+            raise NotEnoughMoney("Solde insuffisant !")
+        print("Produit acheté !")
+        self.wallet = self.wallet - product.price * quantity
